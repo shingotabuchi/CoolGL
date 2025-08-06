d0e915765d8118c4041ec09248a43974f51796e4
Shingo Tabuchi <>
2025-08-06 18:52:46 +0900

    saas

--- src/experiment.cpp
+++ src/experiment.cpp
@@ -3,6 +3,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <SOIL2/SOIL2.h>
 #include <iostream>
 
 const char *vertexShaderSrc = R"glsl(
