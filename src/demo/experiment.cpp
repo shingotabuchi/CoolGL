55aa36cfbaba062e682f8a15c48c50485030943f
Shingo Tabuchi <tabuchishingo123@gmail.com>
2025-08-06 22:54:16 +0900

    rotate

--- src/demo/experiment.cpp
+++ src/demo/experiment.cpp
@@ -129,7 +129,8 @@ int main()
     {
         glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
+        float t = glfwGetTime();
+        model = glm::rotate(glm::mat4(1.0f), t, glm::vec3(0.0f, 1.0f, 0.0f));
         glUseProgram(shaderProgram);
         glm::mat4 mvp = projection * view * model;
         glUniformMatrix4fv(locMVP, 1, GL_FALSE, glm::value_ptr(mvp));
