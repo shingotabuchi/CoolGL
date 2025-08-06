f168e688c7565f0c1acbc0ef436188152f840778
Shingo Tabuchi <tabuchishingo123@gmail.com>
2025-08-07 00:24:13 +0900

    asas

---
+++ src/demo/monkey.cpp
@@ -0,0 +1,171 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <soil2.h>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+#include <iostream>
+
+struct Vertex {
+    glm::vec3 position;
+    glm::vec3 normal;
+};
+
+const char *vertexShaderSrc = R"glsl(
+    #version 410 core
+    layout(location = 0) in vec3 aPos;
+    layout(location = 1) in vec3 aNormal;
+    uniform mat4 uMVP;
+    out vec3 vPos;
+    out vec3 vNormal;
+    void main() {
+        gl_Position = uMVP * vec4(aPos, 1.0);
+        vPos = aPos;
+        vNormal = aNormal;
+    }
+)glsl";
+
+const char *fragmentShaderSrc = R"glsl(
+    #version 410 core
+    out vec4 FragColor;
+    in vec3 vPos;
+    in vec3 vNormal;
+    uniform vec3 uLightPos;
+    uniform vec3 uLightColor;
+    void main() {
+        vec3 lightDir = normalize(uLightPos - vPos);
+        float diff = max(dot(vNormal, lightDir), 0.0);
+        vec3 diffuse = uLightColor * diff;
+        FragColor = vec4(diffuse, 1.0);
+    }
+)glsl";
+
+void framebuffer_size_callback(GLFWwindow *w, int w_, int h)
+{
+    glViewport(0, 0, w_, h);
+}
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow *window = glfwCreateWindow(800, 800, "Cool GL", nullptr, nullptr);
+    if (!window)
+    {
+        std::cerr << "Fail\n";
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cerr << "GLAD init failed\n";
+        return -1;
+    }
+
+    glEnable(GL_DEPTH_TEST);
+
+    Assimp::Importer importer;
+    const aiScene *scene = importer.ReadFile(
+        "resources/monkey.obj", 
+        aiProcess_Triangulate
+    );
+    if(!scene || !scene->HasMeshes()){
+        std::cerr<<"Failed to load mesh\n"; return -1;
+    }
+    aiMesh* m = scene->mMeshes[0];
+
+    std::vector<Vertex> vertices;
+    std::vector<unsigned int> indices;
+    for(unsigned int i = 0; i < m->mNumVertices; i++){
+        Vertex vertex;
+        vertex.position = glm::vec3(m->mVertices[i].x, m->mVertices[i].y, m->mVertices[i].z);
+        vertex.normal = glm::vec3(m->mNormals[i].x, m->mNormals[i].y, m->mNormals[i].z);
+        vertices.push_back(vertex);
+    }
+    for(unsigned int i = 0; i < m->mNumFaces; i++){
+        aiFace face = m->mFaces[i];
+        for(unsigned int j = 0; j < face.mNumIndices; j++){
+            indices.push_back(face.mIndices[j]);
+        }
+    }
+
+    unsigned int VAO, VBO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), vertices.data(), GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), indices.data(), GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    glBindVertexArray(0);
+
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSrc, nullptr);
+    glCompileShader(vertexShader);
+
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSrc, nullptr);
+    glCompileShader(fragmentShader);
+
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    glUseProgram(shaderProgram);
+
+    int locMVP = glGetUniformLocation(shaderProgram, "uMVP");
+    int locLightPos = glGetUniformLocation(shaderProgram, "uLightPos");
+    int locLightColor = glGetUniformLocation(shaderProgram, "uLightColor");
+
+    glm::vec3 lightPos = glm::vec3(0.0f, 0.0f, 10000.0f);
+    glm::vec3 lightColor = glm::vec3(1.0f, 1.0f, 1.0f);
+    glUniform3fv(locLightPos, 1, glm::value_ptr(lightPos));
+    glUniform3fv(locLightColor, 1, glm::value_ptr(lightColor));
+
+    glm::mat4 model = glm::mat4(1.0f);
+    glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -5.0f));
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 800.0f, 0.1f, 100.0f);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        float t = glfwGetTime();
+        model = glm::rotate(glm::mat4(1.0f), t, glm::vec3(0.0f, 1.0f, 0.0f));
+        glm::mat4 mvp = projection * view * model;
+        glUniformMatrix4fv(locMVP, 1, GL_FALSE, glm::value_ptr(mvp));
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
